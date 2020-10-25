29566e02148b37b9a3731ece581880edf71a3fcd
Cristian <13244760+Cristianbcd@users.noreply.github.com>
2020-10-25 17:15:44 -0400

    Assignment 2

--- Assignment 2/Server.java
+++ Assignment 2/Server.java
@@ -8,6 +8,12 @@ import java.net.Socket;
 Please remember, this is just an example there are better and faster ways to code the server
 Use it at your own risk
 */
+
+/*
+netstat -tulpn
+kill <pid>
+*/
+
 public class Server {
 
 	public static void main(String[] args) {
@@ -15,6 +21,7 @@ public class Server {
 		int port = 3001;
 		
 		try {
+			int clientNumber = 1;
 			ServerSocket server = new ServerSocket(port);
 			
 			System.out.println("Server is listening at port " + port);
@@ -44,13 +51,19 @@ public class Server {
 			System.out.println("\nRequest From client");
 			System.out.println(request);
 			
-			String body = "{\"Assignment2\" : \"this is working\"}";
-			
+			//String body = "{\"Client\" : \"" + (clientNumber++) + "\"}";
+			String body = "<!DOCTYPE html><html><body><h2>Assignment 2 Server Example</h2>"
+					+ "<p> You are client number: " + (clientNumber++) +"</p>"
+					+ "<button>Click me</button>"
+					+ "</body>"
+					+ "</html>";
+
 			String response = 	"HTTP/1.0 200 ok\r\n"
 							 	+ "Content-Length: " + body.length() + "\r\n"
 							 	//+ "Content-Disposition: inline "\r\n"
-							 	+ "Content-Disposition: attachment; filename=\"This is working.json\"" + "\r\n"
-								+ "Content-Type: application/json\r\n\r\n"
+							 	//+ "Content-Disposition: attachment; filename=\"ThisWorks.json\"" + "\r\n"
+								//+ "Content-Type: application/json\r\n\r\n"
+								+ "Content-Type: text/html\r\n\r\n"
 			 					+ body;
 			
 			System.out.println("Response sent to client\n" + response);
